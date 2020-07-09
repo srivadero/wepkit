7bae2660c7cc10ac4411da5c64dcf374a899ff29
Sergio Rivadero <rivaderosergio@gmail.com>
2020-07-09 11:28:05 -0300

    new code

--- test/novedad.spec.ts
+++ test/novedad.spec.ts
@@ -4,41 +4,41 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-test.group('Camara', () => {
+test.group('Novedad', () => {
   test('ensure index page works', async (assert) => {
-    const { text } = await supertest(BASE_URL).get('/camara').expect(200)
+    const { text } = await supertest(BASE_URL).get('/novedad').expect(200)
     const { document } = new JSDOM(text).window
     const title = document.querySelector('h1')
     assert.exists(title)
-    assert.equal(title!.textContent!.trim(), 'Gestion de camaras')
+    assert.equal(title!.textContent!.trim(), 'Novedades')
   })
 
   test('ensure create page works', async (assert) => {
-    const { text } = await supertest(BASE_URL).get('/camara/create').expect(200)
+    const { text } = await supertest(BASE_URL).get('/novedad/create').expect(200)
     const { document } = new JSDOM(text).window
     const title = document.querySelector('h1')
     assert.exists(title)
-    assert.equal(title!.textContent!.trim(), 'Nueva camara')
+    assert.equal(title!.textContent!.trim(), 'Ficha de novedad')
   })
 
   test('ensure edit page works', async (assert) => {
-    const { text } = await supertest(BASE_URL).get('/camara/1/edit').expect(200)
+    const { text } = await supertest(BASE_URL).get('/novedad/1/edit').expect(200)
     const { document } = new JSDOM(text).window
     const title = document.querySelector('h1')
     assert.exists(title)
-    assert.equal(title!.textContent!.trim(), 'Edicion de camara')
+    assert.equal(title!.textContent!.trim(), 'Novedad')
   })
 
-  test('ensure show page works', async (assert) => {
-    const { text } = await supertest(BASE_URL).get('/camara/1').expect(200)
+  test.skip('ensure show page works', async (assert) => {
+    const { text } = await supertest(BASE_URL).get('/novedad/1').expect(200)
     const { document } = new JSDOM(text).window
     const title = document.querySelector('h1')
     assert.exists(title)
     assert.equal(title!.textContent!.trim(), 'Camara IP 01')
   })
 
-  test.skip('ensure validator works', async (_assert) =>  { })
-  test.skip('ensure save method works', async (_assert) =>  { })
-  test.skip('ensure update method works', async (_assert) =>  { })
-  test.skip('ensure delete method works', async (_assert) =>  { })
+  // test.skip('ensure validator works', async (_assert) =>  { })
+  // test.skip('ensure save method works', async (_assert) =>  { })
+  // test.skip('ensure update method works', async (_assert) =>  { })
+  // test.skip('ensure delete method works', async (_assert) =>  { })
 })
